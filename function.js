81e1e3f60160ba7ead794af9ac899d9db1092848
Sham2036 <shamkamalesan20@gmail.com>
2024-05-24 10:23:16 +0530

    first commit

---
+++ function.js
@@ -0,0 +1,34 @@
+
+//JavaScript Assignment Operators
+
+var a = 10;
+
+a +=3;
+
+document.write(a,'<br>');
+
+var a = 10;
+
+a -=3;
+
+document.write(a,'<br>')
+var a = 10;
+
+a *=3;
+
+document.write(a,'<br>')
+var a = 10;
+
+a /=3;
+
+document.write(a,'<br>')
+var a = 10;
+
+a %=3;
+
+document.write(a,'<br>')
+var a = 10;
+
+a **=3;
+
+document.write(a,'<br>');
